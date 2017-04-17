c81190d70549f67e0861b641d5db74dec70f25cc
lichaohui <158109640@qq.com>
2017-04-17 10:22:01 +0800

    test

--- wechat/acctoken.js
+++ wechat/acctoken.js
@@ -108,7 +108,7 @@ module.exports=function(option){
          * 如果token有效则通过promise对象的resolve方法将promise对象的状态设置为resolve
          * 就是已完成的状态
          */
-        promise.resolve(data);
+        return promise.resolve(data);
       }else{
         /*
          * 如果token已经过期则还是更新token
