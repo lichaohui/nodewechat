6e421c04ed5615d735499ed4962431dfa9595022
lichaohui <158109640@qq.com>
2017-04-18 21:25:51 +0800

    test

--- config.js
+++ config.js
@@ -10,8 +10,8 @@ const access_token_file=path.join(__dirname,'./config/access_token.json');
 //设置一个对象用来存储一些配置信息
 const config={
   wechat:{
-    appId:'wxbf37d744e196cf9b',
-    appSecret:'fcc5ec54936416d17fb06aba1e3d0d00',
+    appId:'wx2c474296930e81b4',
+    appSecret:'f55e5fa2781afcc869f045830eba269f',
     token:'lichaohui',
     getAccessToken:function(){
       return files.readFileAsync(access_token_file);
