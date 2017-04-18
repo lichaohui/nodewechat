6b1d258759c2d37eab6bf2e5fc7542d36810a8b8
lichaohui <158109640@qq.com>
2017-04-18 11:50:43 +0800

    test

--- wechat/template.js
+++ wechat/template.js
@@ -5,10 +5,10 @@ const [ejs,heredoc]=[require('ejs'),require('heredoc')];
 
 let tpl=heredoc(function(){/*
   <xml>
-    <ToUserName><![CDATA[<%= toUserName %>]]></ToUserName><FromUserName><![CDATA[<%= fromUserName %>]]></FromUserName><CreateTime><%= createTime =></CreateTime><MsgType>
-    <![CDATA[<%= msgType =>]]></MsgType>
+    <ToUserName><![CDATA[<%= toUserName %>]]></ToUserName><FromUserName><![CDATA[<%= fromUserName %>]]></FromUserName><CreateTime><%= createTime %></CreateTime><MsgType>
+    <![CDATA[<%= msgType %>]]></MsgType>
     <% if (msgType=='text'){%>
-      <Content><![CDATA[<%= content =>]]></Content>      
+      <Content><![CDATA[<%= content %>]]></Content>      
     }<% } else if(msgType=='image') { %>
       <Image>
         <MediaId><![CDATA[<%= content.media_id %>]]></MediaId>
