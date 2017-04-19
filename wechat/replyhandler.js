e5282cd4ba5f5c1c50b5a920075052ce15f4f8d7
lichaohui <158109640@qq.com>
2017-04-19 19:18:44 +0800

    test

--- wechat/replyhandler.js
+++ wechat/replyhandler.js
@@ -58,14 +58,14 @@ exports.reply=function* (next){
       break;  
     case 'text':
       //let data=yield material.delete('euOCFj_5eNJC6t4I_eJg-3bGlIniQ5Ry074JZ8-u1WU');
-      let data=yield material.create('temporary','image',path.resolve(__dirname, '..')+'/public/image/foo.jpg');
-      //let data=yield material.show('temporary','euOCFj_5eNJC6t4I_eJg-3bj_ZfwdYGewudyS6XzEl8');
+      //let data=yield material.create('temporary','image',path.resolve(__dirname, '..')+'/public/image/foo.jpg');
+      let data=yield material.show('temporary','QDpEO4ZTPpGN8TeXuPStTuX6oNnIIQlc6CklYOXaBGU5p3xYe72YZaHrNFHvRb-0');
       console.log(data);
-      this.msgType='image';
-      //this.body=data;
-      this.body={
+      this.msgType='text';
+      this.body=data;
+      /*this.body={
         mediaId:data.media_id
-      };
+      };*/
       break;
   }
   yield next;
