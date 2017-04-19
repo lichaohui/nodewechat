393f2986a8c7b5faa04ae2e4db8acd1ac788f1f3
lichaohui <158109640@qq.com>
2017-04-19 19:35:47 +0800

    test

--- wechat/material.js
+++ wechat/material.js
@@ -105,12 +105,11 @@ class material{
       getAcc().then(function(data){
         data=JSON.parse(data);
         //通过switch不同的cate和type来封装不同的上传接口地址和上传数据
-        let [url,option]=['',{}];
+        let [url]=[''];
         switch(cate){
           //获取临时素材  
           case 'temporary':
-            url=`https://api.weixin.qq.com/cgi-bin/media/get?access_token=${data.access_token}`+'&'+`media_id=${media_id}`;
-            //option={url:url,method:'get',json:true};
+            url=`https://api.weixin.qq.com/cgi-bin/media/get?access_token=${data.access_token}%26media_id=${media_id}`;
             break;
           //获取永久素材  
           case 'permanent':
@@ -118,7 +117,6 @@ class material{
             //option={url:url,method:'post',json:true,body:{"media_id":media_id}};
             break;  
         }
-        console.log(url);
         resolve(url);
         /*request(option).then(function(response){
           //响应的数据在response.body中
