e72876bae20f0d6cb8530ca557d6f185d2725355
lichaohui <158109640@qq.com>
2017-04-19 16:47:52 +0800

    test

--- wechat/material.js
+++ wechat/material.js
@@ -120,7 +120,6 @@ class material{
         }
         //resolve(url);
         request(option).then(function(response){
-          console.log(response);
           //响应的数据在response.body中
           let resdata=response.body;
           if(resdata){
