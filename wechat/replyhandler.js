5b0a2caf6c40032b1f77eb37db7af4082c53b312
lichaohui <158109640@qq.com>
2017-04-19 16:21:52 +0800

    test

--- wechat/replyhandler.js
+++ wechat/replyhandler.js
@@ -58,7 +58,7 @@ exports.reply=function* (next){
       break;  
     case 'text':
       //let data=yield material.delete('euOCFj_5eNJC6t4I_eJg-7XgVzc-9u2BInIkqj6otX0');
-      let data=yield material.create('permanent','image',path.resolve(__dirname, '..')+'/public/image/foo.jpg');
+      let data=yield material.create('permanent','other',path.resolve(__dirname, '..')+'/public/image/foo.jpg');
       console.log(data);
       /*if(data.errcode==0){
         this.msgType='text';
