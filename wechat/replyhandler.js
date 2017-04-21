85b1c97c9219603a8492b00e00180c2e81d4e644
lichaohui <158109640@qq.com>
2017-04-21 15:26:17 +0800

    test

--- wechat/replyhandler.js
+++ wechat/replyhandler.js
@@ -12,11 +12,11 @@
 const [material,group,user,news,menu,files,path]=[require('./material'),require('./group'),require('./user'),require('./news'),require('./menu'),require('../libs/files'),require('path')];
 
 //引入菜单数据文件
-files.readFileAsync(path.resolve(__dirname, '..')+'/data/menu.json').then(function(data){
+/*files.readFileAsync(path.resolve(__dirname, '..')+'/data/menu.json').then(function(data){
   return menu.save(JSON.parse(data));
 }).then(function(data){
   console.log(data);
-});
+});*/
 
 exports.reply=function* (next){
   /*
