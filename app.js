3bf76b9a61cacc9fe6a622aa1c1afbea916b417a
lichaohui <158109640@qq.com>
2017-04-22 16:46:08 +0800

    test

--- app.js
+++ app.js
@@ -11,18 +11,26 @@
  * 引入config项目配置文件
  * 引入replyhandler模块
  */
-const[koa,router,reqverify,acctoken,config,reply]=[require('koa'),require('koa-router'),require('./wechat/reqverify'),require('./wechat/acctoken'),require('./config'),require('./wechat/replyhandler')];
+const[koa,route,reqverify,acctoken,config,reply]=[require('koa'),require('koa-route'),require('./wechat/reqverify'),require('./wechat/acctoken'),require('./config'),require('./wechat/replyhandler')];
 
-//实例化一个koa对象
-const app=new koa();
-//app.use(router(app));
-const route=new router();
+/*
+ * 实例化一个koa对象
+ * 实例化route对象
+ */
+const[app,router]=[new koa(),new route()];
+
+//制定路由规则
 route.get('/movie',function(ctx,next){
   console.log(ctx.url);
   ctx.body='hello movie';
+  return next();
+},function(ctx){
+  console.log('到这里来了')；
+  ctx.body='hello movie next';
 })
 
-//app.use(route.routes());
+//在中间件里使用路由
+app.use(route.routes());
 
 //使用acctoken中检验验证access_token
 app.use(acctoken(config.wechat));
