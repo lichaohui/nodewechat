3b94003935fff4903e52e4bb7d6dc5fa3f522ff9
lichaohui <158109640@qq.com>
2017-04-25 16:10:21 +0800

    test

--- app.js
+++ app.js
@@ -115,6 +115,7 @@ router.get('/movie',function(ctx){
     //向下一步返回access_token
     return data.access_token;
   }).then(function(access_token){
+    "use strict";
     //通过ticket对象的getTicket方法传入access_token参数获取ticket
     ticket=yield ticketer.fetchTicket(access_token);
     console.log(ticket);
