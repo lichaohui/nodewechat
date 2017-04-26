73ea1dbf50972ae9293e0d5f12af2b11ada398a6
lichaohui <158109640@qq.com>
2017-04-26 10:22:57 +0800

    test

--- app.js
+++ app.js
@@ -115,7 +115,7 @@ router.get('/movie',function (ctx){
     //向下一步返回access_token
     return data.access_token;
   }).then(function (access_token){
-    ticketer.fetchTicket(access_token);
+    //ticketer.fetchTicket(access_token);
     ctx.body=access_token;
     //通过sign方法传入ticket和this.href参数获取签名
     //let signobj=sign(ticket,ctx.href);
