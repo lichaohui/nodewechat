50846953ff5ebab4cda62b0eb2efe07aaa0f9d4e
lichaohui <158109640@qq.com>
2017-04-27 09:52:39 +0800

    test

--- app.js
+++ app.js
@@ -216,11 +216,11 @@ router.get('/movie',function (ctx){
 //使用acctoken中检验验证access_token
 app.use(acctoken(config.wechat));
 
-//在中间件里使用路由规则
-app.use(router.routes()).use(router.allowedMethods());
-
 //使用reqverify中间件验证请求
 app.use(reqverify(config.wechat,reply.reply));
+
+//在中间件里使用路由规则
+app.use(router.routes()).use(router.allowedMethods());
 
 //监听80端口
 app.listen(80);
